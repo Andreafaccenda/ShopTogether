5da6127dc77635a6230e9eab38885b1e4665f043
aFaccenda <S1096568@studenti.univpm.it>
2022-12-06 17:44:05 +0100

    immagine e nome app finite

--- app/src/main/java/com/example/speedmarket/MainActivity.kt
+++ app/src/main/java/com/example/speedmarket/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shoptogether
+package com.example.speedmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.speedmarket.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/test/java/com/example/speedmarket/ExampleUnitTest.kt
+++ app/src/test/java/com/example/speedmarket/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.shoptogether
+package com.example.speedmarket
 
 import org.junit.Test
 
