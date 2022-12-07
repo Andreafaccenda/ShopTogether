d3bf5545104fdef3764bbf466b544cc70c6f2a2b
aFaccenda <S1096568@studenti.univpm.it>
2022-12-07 10:55:20 +0100

    splash activity inserita

--- app/src/androidTest/java/com/example/speedmarket/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/speedmarket/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.shoptogether", appContext.packageName)
+        assertEquals("com.com.example.speedmarket.shoptogether", appContext.packageName)
     }
 }
---
+++ app/src/main/java/com/example/speedmarket/speedmarket/Activity/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.speedmarket.Activity
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.speedmarket.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
+        else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            },
+            2000
+        )
+    }
+}
+
