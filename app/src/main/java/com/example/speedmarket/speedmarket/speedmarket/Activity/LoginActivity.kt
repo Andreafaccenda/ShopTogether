b6a255164495bd278c96a5739717f1b607ad7894
aFaccenda <S1096568@studenti.univpm.it>
2022-12-07 14:53:13 +0100

    login activity inizio

--- app/src/main/java/com/example/speedmarket/speedmarket/speedmarket/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/speedmarket/speedmarket/speedmarket/Activity/LoginActivity.kt
@@ -1,14 +1,13 @@
-package com.example.speedmarket.Activity
+package com.example.speedmarket.speedmarket.speedmarket.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.speedmarket.R
 
+class LoginActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
-
 }
--- app/src/main/java/com/example/speedmarket/speedmarket/speedmarket/Activity/MainActivity.kt
+++ app/src/main/java/com/example/speedmarket/speedmarket/speedmarket/Activity/MainActivity.kt
@@ -1,19 +1,25 @@
-package com.example.speedmarket.Activity
+package com.example.speedmarket.speedmarket.speedmarket.Activity
 
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.speedmarket.R
+import com.example.speedmarket.databinding.ActivityMainBinding
 
-class SplashActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         }
@@ -26,14 +32,17 @@ class SplashActivity : AppCompatActivity() {
         }
 
 
-        @Suppress("DEPRECATION")
+       /* @Suppress("DEPRECATION")
         Handler().postDelayed(
             {
                 startActivity(Intent(this@SplashActivity, MainActivity::class.java))
                 finish()
             },
             2000
-        )
+        )*/
+        binding.btnAccedi.setOnClickListener(){
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
     }
 }
 
