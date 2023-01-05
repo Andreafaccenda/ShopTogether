eeb271ebfcb730a0e12283f596fb57ed1abbe74f
aFaccenda <S1096568@studenti.univpm.it>
2023-01-05 15:47:16 +0100

    upload image fatto da sistemare

--- app/src/main/java/com/example/speedmarket/ui/impostazioni/profile/Profile.kt
+++ app/src/main/java/com/example/speedmarket/ui/impostazioni/profile/Profile.kt
@@ -1,5 +1,6 @@
 package com.example.speedmarket.ui.impostazioni.profile
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -43,7 +44,12 @@ class Profile : Fragment() {
         startActivityForResult(intent,IMAGE_REQUEST_CODE)
     }
 
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode==RESULT_OK){
+            binding.imageProfile.setImageURI(data?.data)
+        }
+    }
 
 
 
